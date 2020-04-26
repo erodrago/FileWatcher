14eb4a00302d7f33ae64840db085eda048fc8509
Mpesa Dev1 <eric.ndira@gmail.com>
2020-04-27 00:24:03 +0300

    initial commit

---
+++ src/main/java/com/file/watcher/demo/FileWatcherApplication.java
@@ -0,0 +1,13 @@
+package com.file.watcher.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileWatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileWatcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/file/watcher/demo/FileWatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.file.watcher.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileWatcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
